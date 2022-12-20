1dc0b74abdcc5eeebf109364ac1d022a931c3247
Даниил <derendervad@gmail.com>
2022-12-20 18:18:32 +0300

    initial commit

---
+++ Homework_11_1/Homework_11_1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include "Greeter.h"
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::string input = " ";
+    std::cout << "Введите имя: ";
+    std::cin >> input;
+    Greeter g;
+    std::cout << g.greet(input) << std::endl;
+}
